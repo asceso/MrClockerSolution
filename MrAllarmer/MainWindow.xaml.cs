2bd440fc256409cde828044013bed062658a3b62
asceso <asceso111@gmail.com>
2021-04-18 16:07:23 +0300

    Добавьте файлы проекта.

---
+++ MrAllarmer/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace MrAllarmer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer timer = new();
+            timer.Interval = 5000;
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            Hide();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MediaPlayer player1 = new();
+            player1.Open(new Uri("./alarm.mp3", UriKind.Relative));
+            player1.Play();
+        }
+
+        private void CloseAppClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void MinimizeAppClick(object sender, RoutedEventArgs e)
+        {
+            NotifyIcon.Visibility = Visibility.Visible;
+            Hide();
+        }
+        private void ShowAppClick(object sender, RoutedEventArgs e)
+        {
+            Show();
+            NotifyIcon.Visibility = Visibility.Collapsed;
+            if (sender is Button btn)
+            {
+                if (btn.Parent is Grid grd)
+                {
+                    if (grd.Parent is Popup pp)
+                    {
+                        pp.IsOpen = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MrClocker/MrClocker/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MrClocker.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
