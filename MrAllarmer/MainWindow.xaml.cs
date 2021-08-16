e887e8a8f5388056b5d82dc3334b6032a5335f6b
Uchidero Krauch <37419270+asceso@users.noreply.github.com>
2021-08-16 18:31:31 +0300

    first 25-5

--- MrAllarmer/MainWindow.xaml.cs
+++ MrAllarmer/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Notifications.Wpf;
+using System.Media;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 
 namespace MrAllarmer
 {
@@ -12,23 +12,97 @@ namespace MrAllarmer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NotificationManager Notification = new();
         public MainWindow()
         {
             InitializeComponent();
-            Timer timer = new();
-            timer.Interval = 5000;
-            timer.AutoReset = true;
-            timer.Elapsed += Timer_Elapsed;
-            timer.Start();
-            Hide();
         }
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        #region Режим работы 25 - 5
+
+        private const string DisabledString = "Режим ожидания";
+        private const string InChealString = "Отдых";
+        private const string InWorkString = "Работа";
+        private readonly NotificationContent ChealStartContent = new()
         {
-            MediaPlayer player1 = new();
-            player1.Open(new Uri("./alarm.mp3", UriKind.Relative));
-            player1.Play();
+            Message = "До начала работы 5 минут",
+            Title = "Начало отдыха",
+            Type = NotificationType.Information
+        };
+        private readonly NotificationContent ChealEndContent = new()
+        {
+            Message = "Время отдыха закончилось, время работы - 25 минут",
+            Title = "Конец отдыха",
+            Type = NotificationType.Warning
+        };
+        private readonly NotificationContent ChealWorkModeDisabledContent = new()
+        {
+            Message = "Режим 25-5 выключен",
+            Title = "Выключение режима",
+            Type = NotificationType.Success
+        };
+        private WorkChealModeState currentChealWorkModeState = WorkChealModeState.Disabled;
+        private readonly Timer chealModeTimer = new(300000);
+        private readonly Timer workModeTimer = new(1500000);
+
+        private void ChealModeTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (currentChealWorkModeState != WorkChealModeState.Disabled)
+            {
+                ShowFullNotifyWithStatus(ChealEndContent, @"./sounds/wav/cheal_work_mode.wav", InWorkString);
+                chealModeTimer.Stop();
+                chealModeTimer.Elapsed -= ChealModeTimerElapsed;
+                workModeTimer.Elapsed += WorkModeTimerElapsed;
+                workModeTimer.Start();
+            }
         }
+        private void WorkModeTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            if (currentChealWorkModeState != WorkChealModeState.Disabled)
+            {
+                ShowFullNotifyWithStatus(ChealStartContent, @"./sounds/wav/cheal_work_mode.wav", InChealString);
+                workModeTimer.Stop();
+                workModeTimer.Elapsed -= WorkModeTimerElapsed;
+                chealModeTimer.Elapsed += ChealModeTimerElapsed;
+                chealModeTimer.Start();
+            }
+        }
+        private void ChealWorkToggleClick(object sender, RoutedEventArgs e)
+        {
+            switch (ChealWorkModeToggle.IsChecked)
+            {
+                case true:
+                    {
+                        ShowFullNotifyWithStatus(ChealStartContent, @"./sounds/wav/cheal_work_mode.wav", InChealString);
+                        currentChealWorkModeState = WorkChealModeState.InWork;
+                        chealModeTimer.Elapsed += ChealModeTimerElapsed;
+                        chealModeTimer.Start();
+                    }
+                    break;
+                case false:
+                    {
+                        ShowFullNotifyWithStatus(ChealWorkModeDisabledContent, @"./sounds/wav/cheal_work_mode.wav", DisabledString);
+                        currentChealWorkModeState = WorkChealModeState.Disabled;
+                        chealModeTimer.Elapsed -= ChealModeTimerElapsed;
+                        workModeTimer.Elapsed -= WorkModeTimerElapsed;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void ShowFullNotifyWithStatus(NotificationContent content, string pathToSound, string tooltipText)
+        {
+            Notification.Show(content);
+            SoundPlayer player = new();
+            player.SoundLocation = pathToSound;
+            player.LoadAsync();
+            player.Play();
+            Application.Current.Dispatcher.Invoke(() => NotifyIcon.ToolTipText = tooltipText);
+        }
+
+        #endregion
+        #region interface methods
 
         private void CloseAppClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
         private void MinimizeAppClick(object sender, RoutedEventArgs e)
@@ -51,5 +125,12 @@ namespace MrAllarmer
                 }
             }
         }
+
+        #endregion
+    }
+
+    public enum WorkChealModeState
+    {
+        Disabled, InCheal, InWork
     }
 }
